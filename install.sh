578d1155e6092dcd56955b9eb5713b4c6dbe670d
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2021-11-10 16:02:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 git clone git@github.com:dhruvrajvanshi/dotfiles.git $HOME/dotfiles
+
+mkdir -p $HOME/.config/nvim
+
+
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 
