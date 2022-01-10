1a6ded2fc8c533bbafc29dbe01257162e88af9e6
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2022-01-10 02:42:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-
 set -e
+
+if [[ ! -d $HOME/dotfiles ]]; then
+  git clone git@github.com:dhruvrajvanshi/dotfiles.git $HOME/dotfiles
+fi
+
+cd $HOME/dotfiles
+git pull
+cd -
 
 mkdir -p $HOME/.config/nvim
 
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
+if [[ ! -L $HOME/.zshrc ]]; then
+  ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+fi
 
+if [[ ! -L $HOME/.config/nvim/init.vim ]]; then
+  ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
+fi
