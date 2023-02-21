d32569e913b1128eaaa96b54e612a22a93470120
Dhruv <d@rajv.dev>
2023-02-21 20:08:31 +0100

    Configure colorscheme

--- init.lua
+++ init.lua
@@ -2,20 +2,20 @@
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
-  use 'sainnhe/gruvbox-material'
+  use 'morhetz/gruvbox'
   use 'tpope/vim-commentary'
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
 end)
 
 vim.g.mapleader = ' '
 
+vim.cmd.colorscheme('gruvbox')
+
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<C-N>', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+vim.keymap.set('n', '<C-S-N>', telescope.git_files, {})
