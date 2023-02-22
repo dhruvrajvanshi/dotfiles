b8514ba6a227c185fc7435477fd4569f64048190
Dhruv Rajvanshi <d@rajv.dev>
2023-02-22 11:52:40 +0100

    Fix tabstop config

--- init.lua
+++ init.lua
@@ -22,8 +22,10 @@ vim.cmd.colorscheme('gruvbox')
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
 vim.bo.tabstop = 2
-vim.bo.shiftwidth = vim.bo.tabstop
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
 
 local telescope = require('telescope.builtin')
 
