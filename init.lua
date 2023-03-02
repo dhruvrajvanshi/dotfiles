0faa00df7fc1304fdf1099eba7a6aa2b135ffb12
Dhruv Rajvanshi <dhruv.rajvanshi@scalable.capital@Dhruvs-Laptop.local>
2023-03-02 14:54:00 +0100

    Add format shortcut

--- init.lua
+++ init.lua
@@ -9,11 +9,11 @@ require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-  use({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
+  use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
 
   use 'airblade/vim-gitgutter'
 
@@ -22,20 +22,20 @@ require('packer').startup(function(use)
     branch = 'v1.x',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      { 'neovim/nvim-lspconfig' }, -- Required
+      { 'williamboman/mason.nvim' }, -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' }, -- Optional
+      { 'hrsh7th/cmp-path' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
+      { 'L3MON4D3/LuaSnip' }, -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
     }
   }
 
@@ -71,7 +71,7 @@ vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<C-S-N>', telescope.git_files, {})
 vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "c", "lua", "vim", "help" },
 
   auto_install = true,
@@ -89,15 +89,15 @@ local lsp = require('lsp-zero').preset({
 })
 
 vim.keymap.set('n', 'gh', vim.lsp.buf.hover)
+vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end)
 
-
+vim.diagnostic.config({ virtual_text = true })
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   vim.lsp.handlers.hover,
-  {border = 'rounded'}
+  { border = 'rounded' }
 )
 
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 lsp.setup()
-
