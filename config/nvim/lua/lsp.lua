fa832caa78964ad1d92c25c05e1caf9630208d66
Dhruv Rajvanshi <d@rajv.dev>
2025-01-25 11:25:07 +0100

    Set up keybinds for going to definition

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -9,3 +9,16 @@ require"cmp".setup {
     { name = "nvim_lsp" },
   }
 }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client.supports_method('textDocument/rename') then
+        -- Create a keymap for vim.lsp.buf.rename()
+        vim.keymap.set('n', '<S-R>', vim.lsp.buf.rename)
+      end
+      if client.supports_method('textDocument/definition') then
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+      end
+    end,
+  })
