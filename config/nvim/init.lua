be45ab62755c32bd66ca68be6d928a4228eaf2e7
Dhruv Rajvanshi <d@rajv.dev>
2025-02-04 11:19:25 +0100

    Configure auto formatting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,85 +1,86 @@
 require("bootstrap-lazy")
 
 require("lazy").setup({
-  -- Colorschemes
-  { "morhetz/gruvbox" },
-  { "loctvl842/monokai-pro.nvim" },
+	-- Colorschemes
+	{ "morhetz/gruvbox" },
+	{ "loctvl842/monokai-pro.nvim" },
 
-  -- Provides syntax highlighting for various languages; Set to auto install when a buffer
-  -- is opened
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	-- Provides syntax highlighting for various languages; Set to auto install when a buffer
+	-- is opened
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-  -- Comment a line using gcc, or any object using gc<motion>, e.g. gci{
-  { "tpope/vim-commentary" },
+	-- Comment a line using gcc, or any object using gc<motion>, e.g. gci{
+	{ "tpope/vim-commentary" },
 
-  -- Provides fuzzy search; Set to C-Shift-N
-  { "junegunn/fzf", build = ":call fzf#install()" },
-  { "junegunn/fzf.vim" },
+	-- Provides fuzzy search; Set to C-Shift-N
+	{ "junegunn/fzf", build = ":call fzf#install()" },
+	{ "junegunn/fzf.vim" },
 
-  -- Show git changes in the gutter
-  { "airblade/vim-gitgutter" },
-  { "tpope/vim-fugitive" },
-  { "beauwilliams/statusline.lua" },
+	-- Show git changes in the gutter
+	{ "airblade/vim-gitgutter" },
+	{ "tpope/vim-fugitive" },
+	{ "beauwilliams/statusline.lua" },
 
-  { "github/copilot.vim" },
+	{ "github/copilot.vim" },
 
-  {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    {
-      'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
-      dependencies = 'rafamadriz/friendly-snippets',
+	{ "mhartington/formatter.nvim" },
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		{
+			"saghen/blink.cmp",
+			-- optional: provides snippets for the snippet source
+			dependencies = "rafamadriz/friendly-snippets",
 
-      -- use a release tag to download pre-built binaries
-      version = '*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
+			-- use a release tag to download pre-built binaries
+			version = "*",
+			-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+			-- build = 'cargo build --release',
+			-- If you use nix, you can build from source using latest nightly rust with:
+			-- build = 'nix run .#build-plugin',
 
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-        -- See the full "keymap" documentation for information on defining your own keymap.
-        keymap = { preset = 'enter'},
+			---@module 'blink.cmp'
+			---@type blink.cmp.Config
+			opts = {
+				-- 'default' for mappings similar to built-in completion
+				-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+				-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+				-- See the full "keymap" documentation for information on defining your own keymap.
+				keymap = { preset = "enter" },
 
-        appearance = {
-          -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-          -- Useful for when your theme doesn't support blink.cmp
-          -- Will be removed in a future release
-          use_nvim_cmp_as_default = true,
-          -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono'
-        },
+				appearance = {
+					-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+					-- Useful for when your theme doesn't support blink.cmp
+					-- Will be removed in a future release
+					use_nvim_cmp_as_default = true,
+					-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+					-- Adjusts spacing to ensure icons are aligned
+					nerd_font_variant = "mono",
+				},
 
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-          -- disabling this in the command line because
-          -- when keymap is set to enter, :q<Enter> will automatically pick the
-          -- first completion item
-          cmdline = {}
-        },
-      },
-      opts_extend = { "sources.default" }
-    }
-  }
+				-- Default list of enabled providers defined so that you can extend it
+				-- elsewhere in your config, without redefining it, due to `opts_extend`
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+					-- disabling this in the command line because
+					-- when keymap is set to enter, :q<Enter> will automatically pick the
+					-- first completion item
+					cmdline = {},
+				},
+			},
+			opts_extend = { "sources.default" },
+		},
+	},
 })
 
 vim.diagnostic.config({
-  virtual_text = false
+	virtual_text = false,
 })
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.cmd.colorscheme('monokai-pro')
+vim.cmd.colorscheme("monokai-pro")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -92,24 +93,42 @@ vim.o.scrolloff = 12
 
 vim.opt.termguicolors = true
 
-vim.keymap.set('n', '<leader>ff', ':Files<CR>', {})
-vim.keymap.set('n', '<C-S-N>', ':GFiles<CR>', {})
-vim.keymap.set('n', '<C-S-F>', ':Rg<CR>')
+vim.keymap.set("n", "<leader>ff", ":Files<CR>", {})
+vim.keymap.set("n", "<C-S-N>", ":GFiles<CR>", {})
+vim.keymap.set("n", "<C-S-F>", ":Rg<CR>")
 
-require 'nvim-treesitter.configs'.setup {
-  auto_install = true,
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
 
-  highlight = {
-    enable = true,
-  },
-}
+	highlight = {
+		enable = true,
+	},
+})
 
 -- https://neovim.io/doc/user/diagnostic.html
 vim.diagnostic.config({
-  -- Show the diagnostic message on the line where the error is
-  -- Without this, the diagnostic will be underlined, but there's
-  -- no obvious way to read the message
-  virtual_text = false,
+	-- Show the diagnostic message on the line where the error is
+	-- Without this, the diagnostic will be underlined, but there's
+	-- no obvious way to read the message
+	virtual_text = false,
 })
 
-require "lsp"
+require("lsp")
+require("formatter").setup({
+	filetype = {
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+	},
+})
+vim.keymap.set("n", "<leader>pp", ":Format<CR>")
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -3,6 +3,11 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 require'lspconfig'.ts_ls.setup {}
+require'lspconfig'.prettier.setup {
+  settings = {
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss", "json", "graphql", "markdown", "vue" }
+  }
+}
 require'lspconfig'.rust_analyzer.setup {}
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -11,9 +16,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
       if client.supports_method('textDocument/rename') then
         -- Create a keymap for vim.lsp.buf.rename()
         vim.keymap.set('n', '<S-R>', vim.lsp.buf.rename)
+	vim.keymap.set('n', '<F2>', vim.lsp.buf.rename)
       end
       if client.supports_method('textDocument/definition') then
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
       end
+
+      if client.supports_method('textDocument/formatting') and client.name ~= 'ts_ls' then
+        -- Format the current buffer on save
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+          end,
+        })
+      end 
     end,
   })
