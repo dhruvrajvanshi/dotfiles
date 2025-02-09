c31812ab6174fb1836818b55ed019d82378a74ff
Dhruv Rajvanshi <d@rajv.dev>
2025-02-09 15:40:37 +0100

    Add treesitter textobjects

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -46,6 +46,8 @@ require("lazy").setup({
 		},
 		config = true,
 	},
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+
 	{ "beauwilliams/statusline.lua" },
 
 	{ "github/copilot.vim" },
@@ -132,6 +134,26 @@ require("nvim-treesitter.configs").setup({
 
 	highlight = {
 		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
+
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				-- You can optionally set descriptions to the mappings (used in the desc parameter of
+				-- nvim_buf_set_keymap) which plugins like which-key display
+				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+				-- You can also use captures from other query groups like `locals.scm`
+				["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+			},
+		},
 	},
 })
 
