b07d3814f49933d5f0625ef6902c75708af74689
Dhruv Rajvanshi <d@rajv.dev>
2025-02-10 11:02:23 +0100

    Add keybindings for go to references

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -12,10 +12,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			-- Create a keymap for vim.lsp.buf.rename()
 			vim.keymap.set("n", "<S-R>", vim.lsp.buf.rename)
 			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+			vim.keymap.set("n", "grn", vim.lsp.buf.rename)
 		end
 		if client.supports_method("textDocument/definition") then
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 			vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+		end
+
+		if client.supports_method("textDocument/references") then
+			vim.keymap.set("n", "grr", vim.lsp.buf.references)
+		end
+
+		if client.supports_method("textDocument/implementation") then
+			vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
 		end
 
 		if client.supports_method("textDocument/formatting") and client.name ~= "ts_ls" then
