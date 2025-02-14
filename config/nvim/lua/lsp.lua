538cac6d48033f4fa88765b2427ee9b7785a473f
Dhruv Rajvanshi <d@rajv.dev>
2025-02-14 21:29:37 +0100

    Add descriptions for keybindings

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -11,22 +11,38 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.supports_method("textDocument/rename") then
 			-- Create a keymap for vim.lsp.buf.rename()
-			vim.keymap.set("n", "<S-R>", vim.lsp.buf.rename)
-			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
-			vim.keymap.set("n", "grn", vim.lsp.buf.rename)
+			vim.keymap.set("n", "<S-R>", vim.lsp.buf.rename, {
+				desc = "LSP Rename",
+			})
+			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {
+				desc = "LSP Rename",
+			})
+			vim.keymap.set("n", "grn", vim.lsp.buf.rename, {
+				desc = "LSP Rename",
+			})
 		end
 		if client.supports_method("textDocument/definition") then
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-			vim.keymap.set("n", "gh", vim.lsp.buf.hover)
-			vim.keymap.set("n", "geh", vim.diagnostic.open_float)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+				desc = "LSP go to definition",
+			})
+			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {
+				desc = "LSP hover",
+			})
+			vim.keymap.set("n", "geh", vim.diagnostic.open_float, {
+				desc = "LSP error hover",
+			})
 		end
 
 		if client.supports_method("textDocument/references") then
-			vim.keymap.set("n", "grr", vim.lsp.buf.references)
+			vim.keymap.set("n", "grr", vim.lsp.buf.references, {
+				desc = "LSP go to references",
+			})
 		end
 
 		if client.supports_method("textDocument/implementation") then
-			vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
+			vim.keymap.set("n", "gri", vim.lsp.buf.implementation, {
+				desc = "LSP go to implementation",
+			})
 		end
 	end,
 })
