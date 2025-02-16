d6f051a3eebfc691561c70009103d607b901bb40
Dhruv Rajvanshi <d@rajv.dev>
2025-02-16 21:31:22 +0100

    Add a keymap for opening a telescope for lsp symbols

--- config/nvim/lua/plugins/all.lua
+++ config/nvim/lua/plugins/all.lua
@@ -29,7 +29,7 @@ return {
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<C-S-F>", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope lsp document symbols" })
 		end,
 	},
 
