d9ef142528c5ddf4c749bec11d555b0d25229df5
Dhruv Rajvanshi <d@rajv.dev>
2025-02-16 18:55:47 +0100

    Set a keybinding for helptags

--- config/nvim/lua/plugins/all.lua
+++ config/nvim/lua/plugins/all.lua
@@ -26,6 +26,7 @@ return {
 			vim.keymap.set("n", "<C-S-N>", builtin.git_files, { desc = "Telescope git files" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<C-S-F>", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
