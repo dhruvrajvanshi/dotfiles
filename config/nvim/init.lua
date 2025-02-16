fd1dd40bcf2518846603f630b6076f223707c0cf
Dhruv Rajvanshi <d@rajv.dev>
2025-02-16 11:20:24 +0100

    Turn off relative line numbers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,7 +4,6 @@ require("lazy").setup("plugins")
 
 vim.opt.shiftwidth = 2
 vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
