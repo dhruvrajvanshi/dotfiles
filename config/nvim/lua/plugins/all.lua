f7ba4e263e027850606ad7dec6f9764f139e5349
Dhruv Rajvanshi <d@rajv.dev>
2025-03-29 08:35:22 +0100

    Move telescope config to a separate file

--- config/nvim/lua/plugins/all.lua
+++ config/nvim/lua/plugins/all.lua
@@ -12,27 +12,6 @@ return {
 	-- Adds :GBrowse command for interacting with github
 	{ "tpope/vim-rhubarb" },
 	{ "folke/which-key.nvim" },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-			},
-		},
-		init = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-S-N>", builtin.git_files, { desc = "Telescope git files" })
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-			vim.keymap.set("n", "<C-S-F>", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope lsp document symbols" })
-		end,
-	},
 
 	-- Shows the :command line in a floating window
 	{
