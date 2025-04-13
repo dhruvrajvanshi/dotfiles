ee350ae1f6632546c0e1ee142aef2a6c5a503578
Dhruv Rajvanshi <d@rajv.dev>
2025-04-13 19:53:12 +0200

    Add a keybinding for toggling neotree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -6,4 +6,7 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
+	init = function()
+		vim.keymap.set("n", "<leader>b", "<cmd>Neotree toggle<cr>", { desc = "Toggle NeoTree" })
+	end,
 }
