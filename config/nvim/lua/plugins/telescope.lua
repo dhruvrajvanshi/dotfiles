bc931df018c8c670c6d9e7acfc8c177161970c13
Dhruv Rajvanshi <d@rajv.dev>
2025-07-09 14:55:25 +0200

    Improve C-S-F window

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -21,12 +21,25 @@ return {
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<C-S-F>", function()
-				builtin.live_grep({ grep_open_files = true })
+				builtin.live_grep({
+					layout_strategy = "horizontal",
+					path_display = { "truncate" },
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							width = { padding = 5 },
+							preview_width = 0.5,
+						},
+					},
+				})
 			end, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope lsp document symbols" })
 		end,
 		opts = {
+			defaults = {
+				dynamic_preview_title = true,
+			},
 			pickers = {
 				find_files = {
 					theme = "dropdown",
